2c446252e4442afb8c6d6c7efc2e1c9d963632cc
hellboykelevra <silver.correo@gmail.com>
2024-01-18 14:03:18 +0000

    Initial commit with start project

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+/* .................................................. */
+/* EN ESTE ESPACIO PODEMOS PONER SOLAMENTE JAVASCRIPT */
+/* .................................................. */
+/* Made by Silver (IES EL RINCON - 2024) */
+console.log("%c:::::::::::::::::::::::::::::::::::::::::::::::", 'background: #222; color: #bada55');
+console.log("%c:: EN ESTE ESPACION PRACTICAREMOS JAVASCRIPT ::", 'background: #222; color: #bada55');
+console.log("%c:::::::::::::::::::::::::::::::::::::::::::::::", 'background: #222; color: #bada55');
+
+// Esto es una variable
+var mi_variable = 3;
